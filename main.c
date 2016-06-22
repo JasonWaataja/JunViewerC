98d0bc5b0f818af3c5b7548a4029acb30aa50ec3
Jason Waataja <jasonswaataja@gmail.com>
2016-06-22 11:35:29 -0700

    Updated copyright stuff

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
-/*   This file is part of JunViewerC.
+/*
+Copyright (C) 2016 Jason Waataja
 
-     JunViewerC is free software: you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
-     the Free Software Foundation, either version 3 of the License, or
-     (at your option) any later version.
+This file is part of JunViewerC.
 
-     JunViewerC is distributed in the hope that it will be useful,
-     but WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     GNU General Public License for more details.
+JunViewerC is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-     You should have received a copy of the GNU General Public License
-     along with JunViewerC.  If not, see <http://www.gnu.org/licenses/>.  */
+JunViewerC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with JunViewerC.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include <gtk/gtk.h>
 #include <stdio.h>
